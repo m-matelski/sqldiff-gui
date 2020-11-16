a7977405b8716148dcf4d461fc023777bd3d8341
Mateusz <m.z.matelski@gmail.com>
2020-11-16 20:04:24 +0100

    windows fullscreen

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ import tkinter as tk
 if __name__ == '__main__':
     mw = SqlCompareDualTextWindow()
     mw.title('Sql Diff')
-    mw.attributes('-zoomed', True)
+    # mw.attributes('-zoomed', True)
+    # mw.attributes('-fullscreen', True)
+    # fullscreen on windows
+    mw.wm_state('zoomed')
     # mw.mainloop()
     # mw.iconbitmap(os.path.join('gui/resources/icons/sql_diff_icon.ico'))
     # mw.iconbitmap(os.path.join('gui/resources/icons/32x32/switch.png'))
     # imgicon = tk.PhotoImage(file=os.path.join('gui/resources/icons/32x32/switch.png'))
-    # imgicon = tk.PhotoImage(file=os.path.join('gui/resources/icons/sql_diff_icon.png'))
-    # mw.tk.call('wm', 'iconphoto', mw._w, imgicon)
+    imgicon = tk.PhotoImage(file=os.path.join('gui/resources/icons/sql_diff_icon.png'))
+    mw.tk.call('wm', 'iconphoto', mw._w, imgicon)
     mw.mainloop()
